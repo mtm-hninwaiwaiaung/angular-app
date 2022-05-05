d8eef90e8cd55baae9e47d62c0508dee9ea186ed
hninwaiwaiaung <hnin.waiwaiaung@seattleconsultingmyanmar.com>
2022-05-05 10:42:34 +0630

    commit for adding new format in password validation checking.

--- src/app/pages/assignment-day12/assignment-day12.component.ts
+++ src/app/pages/assignment-day12/assignment-day12.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-import { MustMatch } from '../../validators/must-match.validator';
-
 @Component({
   selector: 'app-assignment-day12',
   templateUrl: './assignment-day12.component.html',
@@ -20,8 +18,6 @@ export class AssignmentDay12Component implements OnInit {
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
       confirmPassword: ['', Validators.required]
-    }, {
-      validator: MustMatch('password', 'confirmPassword')
     });
   }
 
@@ -31,5 +27,19 @@ export class AssignmentDay12Component implements OnInit {
 
   onSubmit() {
     alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registrationForm.value));
+  }
+
+  checkConfirmPassword() {
+    const matchingControl = this.registrationForm.controls['confirmPassword'];
+    if (matchingControl.errors && !matchingControl.errors['mustMatch']) {
+      // return if another validator has already found an error on the matchingControl.
+      return;
+    }
+    if ((this.registrationForm.value.password !== this.registrationForm.value.confirmPassword) &&
+      this.registrationForm.value.password !== '' && this.registrationForm.value.confirmPassword !== '') {
+      matchingControl.setErrors({ mustMatch: true });
+    } else {
+      matchingControl.setErrors(null);
+    }
   }
 }
